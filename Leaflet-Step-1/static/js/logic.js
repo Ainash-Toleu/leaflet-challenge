fba6f440b65636cdb32b8dc1bee5d5357eb54fe8
Ainash Toleu <ainashtoleu@Ainashs-MacBook-Air.local>
2021-05-24 20:59:00 -0500

    added first marker

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,21 +1,4 @@
-// Creating our initial map object
-// We set the longitude, latitude, and the starting zoom level
-// This gets inserted into the div with an id of 'map'
-var myMap = L.map("map", {
-  center: [45.52, -122.67],
-  zoom: 13
-});
 
-// Adding a tile layer (the background map image) to our map
-// We use the addTo method to add objects to our map
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
 
 url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 // Perform a GET request to the query URL
@@ -28,6 +11,37 @@ d3.json(url).then(function(data) {
   console.log(data.features[0].geometry.coordinates[0]);
   console.log(data.features[0].geometry.coordinates[1]);
 
+  // Creating our initial map object
+  // We set the longitude, latitude, and the starting zoom level
+  // This gets inserted into the div with an id of 'map'
+  var myMap = L.map("map", {
+    center: [35.926, -117.7115],
+   zoom: 13
+  });
+
+  // Adding a tile layer (the background map image) to our map
+  // We use the addTo method to add objects to our map
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  // Create a new marker
+  // Pass in some initial options, and then add it to the map using the addTo method
+  var marker = L.marker([data.features[0].geometry.coordinates[1], data.features[0].geometry.coordinates[0]], {
+    draggable: true,
+    title: "Earthquake"
+  }).addTo(myMap);
+
+  // Binding a pop-up to our marker
+  marker.bindPopup(data.features[0].properties.title);
+
 });
+
+
+
 
 
