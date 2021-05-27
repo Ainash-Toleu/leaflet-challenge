2066428737092307cfe3a2b756507fd12c27111b
Ainash Toleu <ainashtoleu@Ainashs-MacBook-Air.local>
2021-05-27 18:40:02 -0500

    made some changes in circle code

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -5,7 +5,7 @@ function getColor(d) {
          d > 30  ? '#ffeda0' :
          d > 10   ? '#f7fcb9' :
          d > -10   ? '#addd8e' :  
-                    '#FFEDA0';
+                    '636363';
 }
 
 url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
@@ -47,33 +47,49 @@ d3.json(url).then(function(data) {
     console.log(i, lat, lng, depth, title, mag);
     // var circle = L.circle([lat, lng]).addTo(myMap);
     var circle = L.circle([lat, lng], {
-      color: getColor(depth),
+      color: 'black',
       fillColor: getColor(depth),
-      fillOpacity: 0.5,
-      weight: mag*10,
-      radius: 500}).addTo(myMap);
+  
+      fillOpacity: 1,
+      weight: 0.5,
+      radius: mag*20000}).addTo(myMap);
     circle.bindPopup(title);
     // var marker = L.marker([lat, lng]).addTo(myMap);
     // marker.bindPopup(title);
   };
 
-  
-  var legend = L.control({position: 'bottomright'});  
-    legend.onAdd = function (map) {
+  var legend = L.control({position: 'bottomright'});
 
-      var div = L.DomUtil.create('div', 'info legend'),
-      grades = [-10, 10, 30, 50, 70, 90],
-      labels=[];
 
-      for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-      }
+  legend.onAdd = function (myMap) {
+    var legendDiv =  L.DomUtil.create('div', 'info legend'),
+    grades = [-10, 10, 30, 50, 70, 90],
+    labels = [];
+    for ( var i=0; i < grades.length; i++) {
+      labels.push('<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+      grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+'));
+    }
+    legendDiv.innerHTML = labels.join('<br>');
+    return legendDiv;
+  }
+
+  legend.addTo(myMap);
+  // var legend = L.control({position: 'bottomright'});  
+  //   legend.onAdd = function (map) {
+
+  //     var div = L.DomUtil.create('div', 'info legend'),
+  //     grades = [-10, 10, 30, 50, 70, 90],
+  //     labels=[];
+
+  //     for (var i = 0; i < grades.length; i++) {
+  //       div.innerHTML +=
+  //           '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+  //           grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+  //     }
     
-      return div;
-    };
-    legend.addTo(map);
+  //     return div;
+  //   };
+  //   legend.addTo(map);
 
   
 
